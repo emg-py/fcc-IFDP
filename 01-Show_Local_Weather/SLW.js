5e85d33f58c37cf01c4e6df4f7b1d885839b5cbe
emg-py <github@emg.ovh>
2017-09-13 16:58:41 +0200

    première version de l'application 'météo'

--- 01-Show_Local_Weather/SLW.js
+++ 01-Show_Local_Weather/SLW.js
@@ -5,9 +5,14 @@
 une page présentant soit une page aléatoire de l'encyclopédie Wikipedia, soit une liste d'articles correspondant 
 à une recherche validée dans un formulaire.</p>
 <p>Les recherches seront effectuées sur la version française de l'encyclopédie au moyen de l'api Wikimedia.</p>
+
+1 - je peux consulter la météo d'où je suis
+2 - je peux voir une icône illustrant ces conditions
+3 - je peux basculer l'affichage des températures de °C à °F par un contrôle
+
 */
 
-var apiURL = "https://fr.wikipedia.org/w/api.php?callback=?";
+var apiURL = "https://fcc-weather-api.glitch.me/api/current?";
 
 $(document).ready(function() {
 /* validation d'une recherche par un retour chariot */
@@ -16,30 +21,45 @@ $(document).ready(function() {
         $("#boutonRechercher").click();
       }
     });
+
+
+
+
+
 /* fonction de recherche et d'affichage */
-    $('#boutonRechercher').click(function() {
-      $("#resultatsAfficher").empty(); // effacer les résultats d'une recherche précédente
+    $('#geoLocation').click(function() {
+      navigator.geolocation.getCurrentPosition(function(position) {
         $.getJSON(apiURL, {
-          action: 'query',
-          format: 'json',
-          inprop: "url",
-          formatversion: 2,
-          generator: 'search',
-          gsrsearch: $("input").val(),
-          gsrwhat: "text",
-          prop: 'extracts|info',
-          exsentences: 3,
-          exintro: "",
-          explaintext: "",        
-          exlimit: 20,
+          lon: position.coords.longitude,
+          lat: position.coords.latitude,
         })
         .done(function(response) {
-          console.log(response);
-          response.query.pages.forEach(function(resp) {
-            $('#resultatsAfficher').append(
-              "<a href='" + resp.fullurl + "' target= '_blank'><div id='result' class='results'><h3>" + resp.title + "</h3></a><p = class='extract'>" + resp.extract + "</p></div>");
-          })
+       //   console.log(response);
+          $("#resultatsAfficher").html("Vous vous trouvez à proximité de " + response.name);
+      // la longueur de la liste 'weather' varie, la référence de l'icône est indiquée dans le dernier élément (0,1, 2 ou 3)
+      var refIcone = response.weather.length - 1;
+      console.log(refIcone); 
+          $("#icone").html('<img src="http://openweathermap.org/img/w/' + response.weather[refIcone].icon + '.png" alt="icone météo"></img>');
+
+      /* Calcul température en °F / °C */
+console.log(response);
+          var tempF = response.main.temp * (9/5) + 32;
+          tempF = Math.round(tempF);
+          var tempC = Math.round(response.main.temp);
+          console.log(tempF);
+          $("#temperature").html("Il fait : " + tempC + "°C");
+          $("#farhenheit").html("Il fait : " + tempF + " °F");
+
+
+//  soit : " + (response.main.temp * (9/5) + 32) + " °F"
         });
+         // effacer les résultats d'une recherche précédente
+          //      do_something(position.coords.latitude, position.coords.longitude);
+      });
+
+
+    //  $("#resultatsAfficher").empty(); // effacer les résultats d'une recherche précédente
+
     }); // search
 
 });
